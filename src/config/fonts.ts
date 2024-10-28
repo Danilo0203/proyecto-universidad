abf3f3faea3411394865a3f2600d5177e6a8897c
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-10-28 00:18:19 -0600

    first commit

---
+++ src/config/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Montserrat_Alternates, Poppins } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const titleFont = Montserrat_Alternates({ subsets: ["latin"], weight: ["500", "700"] });
+export const poppins = Poppins({ subsets: ["latin"], weight: ["100", "400", "700"] });
---
+++ src/providers.tsx
@@ -0,0 +1,9 @@
+import { NextUIProvider } from "@nextui-org/system";
+
+export default function Providers({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
